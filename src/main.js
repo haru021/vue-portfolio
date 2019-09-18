0aa7c14b9f5c67d644a901b62eb6aec032f8b6cc
小林　萌 <haru.gt.021@gmail.com>
2019-09-18 16:55:02 +0900

    bootstrap実装テスト失敗

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router.js'
+
 new Vue({
   el: '#app',
   router, 
   render: h => h(App)
 })
+ 
+Vue.config.productionTip = false
+ 
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
