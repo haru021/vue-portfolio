c915837ebc0c3b7acd4c47b610e7c2b3fde5875a
小林　萌 <haru.gt.021@gmail.com>
2019-09-19 11:50:10 +0900

    インポート記述ミス修正

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(BootstrapVue)
