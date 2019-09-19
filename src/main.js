013e550ee7eb288ff7ba465ea4cc692d3d5f30d0
小林　萌 <haru.gt.021@gmail.com>
2019-09-19 11:49:25 +0900

    bootstrapインポート記述変更

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
 import router from './router.js'
 
 new Vue({
@@ -11,8 +16,3 @@ new Vue({
   render: h => h(App)
 })
  
-Vue.config.productionTip = false
- 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
