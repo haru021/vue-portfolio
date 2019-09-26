58c51a806a95d6b2bb646923a652dbd7eaa53239
小林　萌 <haru.gt.021@gmail.com>
2019-09-26 16:53:10 +0900

    自己紹介実装

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-<div id="app">  
-  <header>
+<div id="app">
+  <b-container class="header">
+    <b-row>
+      <b-col cols="12">  
         <h1 class="bg-dark">Moe'sPorfolio</h1>
         <nav class="navbar navbar-dark bg-dark">
       <b-button v-b-modal.modal-1 class="navbar-toggler"><span class="navbar-toggler-icon"></span></b-button>
   <b-modal id="modal-1" title="BootstrapVue">
     <p class="my-4">Hello from modal!</p>
   </b-modal>
-  </nav>
-        </header>  
+  </nav> 
+  </b-col>
+  </b-row>
+        </b-container>
         <router-view />
         </div>
 </template>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="product">
-        <h1>商品情報</h1>
-    </div>
+    <b-container class="product">
+        <b-row>
+        <b-col cols="12">
+            <h1 class="text-center">自己紹介</h1>
+        <b-card-group deck>
+            <b-card header="基本情報" class="text-left">
+        <b-card-text><h2>小林萌</h2>
+        <p>生年月日：1992年9月1日</p>
+        <p>年齢：27歳</p>
+        <p>学歴：九州産業大学国際文化学部日本文化学科2016年卒業</p>
+        <p>職歴：携帯販売・Webデザイナー</p>
+        </b-card-text>
+      </b-card>
+        </b-card-group>
+        </b-col>
+        </b-row>
+        </b-container>
 </template>
