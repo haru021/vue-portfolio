6f42bcd7761711af8efcc06e19921285542dba2d
小林　萌 <haru.gt.021@gmail.com>
2019-09-26 11:53:44 +0900

    ヘッダー背景変更

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app">  
   <header>
-        <h1>Moe'sPorfolio</h1>
+        <h1 class="bg-dark">Moe'sPorfolio</h1>
         <nav class="navbar navbar-dark bg-dark">
       <b-button v-b-modal.modal-1 class="navbar-toggler"><span class="navbar-toggler-icon"></span></b-button>
   <b-modal id="modal-1" title="BootstrapVue">
