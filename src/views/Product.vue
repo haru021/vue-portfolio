400431b9270d44039100de87577022d87dbd54e7
小林　萌 <haru.gt.021@gmail.com>
2019-09-29 14:10:17 +0900

    自己紹介ベージ全項目追加

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -23,6 +23,39 @@
         </b-card-text>
       </b-card>
         </b-card-group>
+        <b-card-group deck>
+            <b-card header="資格" class="text-left">
+        <b-card-text>
+        <p>WEB関係：</p>
+        <p>Webクリエイター能力認定試験エキスパート</p>
+        <p>その他：</p>
+        <p>語彙・読解力検定２級</p>
+        </b-card-text>
+      </b-card>
+        </b-card-group>
+        <b-card-group deck>
+            <b-card header="職務経歴" class="text-left">
+        <b-card-text>
+        <p>携帯販売：</p>
+        <p>回線の新規契約や契約内容の案内、更新、機種変更などの窓口業務や付属品販売などを行なっていました。</p>
+        <p>レディースアパレルECショップ：</p>
+        <p>楽天に出店している店舗のサイト管理や新規イベントページ作成などコーディングをメインに業務を行なっていました。</p>
+        <p>化粧品ECサイト：</p>
+        <p>自社で運営しているECサイトの保守管理やLP製作時のディレクションや広告代理店とのやりとりを行なっていました。</p>
+        </b-card-text>
+      </b-card>
+        </b-card-group>
+        <b-card-group deck>
+            <b-card header="趣味嗜好" class="text-left">
+        <b-card-text>
+        <p>趣味：車</p>
+        <p>好きな車：</p>
+        <p>HONDA シビックTYPE R、三菱　ランサーエボリューション、トヨタ カムリ、アウディ A4、BMW 3シリーズ</p>
+        <p>好きなドライブコース：</p>
+        <p>飯塚〜北九州、朝倉〜大分、阿蘇山一周、佐世保、長崎市、糸島〜唐津</p>
+        </b-card-text>
+      </b-card>
+        </b-card-group>
         </b-col>
         </b-row>
         </b-container>
