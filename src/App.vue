3c8c984b1c8b520b1e13dbcccb9b2467667284ae
小林　萌 <haru.gt.021@gmail.com>
2019-10-02 16:53:41 +0900

    モーダルナビゲーションに項目追加

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,10 @@
       <b-button v-b-modal.modal-1 class="navbar-toggler"><span class="navbar-toggler-icon"></span></b-button>
   <b-modal id="modal-1" hide-footer="true">
     <b-list-group>
-  <b-list-group-item><a href="#"></a></b-list-group-item>
-  <b-list-group-item><a href="#"></a></b-list-group-item>
-  <b-list-group-item><a href="#"></a></b-list-group-item>
-  <b-list-group-item><a href="#"></a></b-list-group-item>
-  <b-list-group-item><a href="#"></a></b-list-group-item>
+  <b-list-group-item><a href="#">Contact</a></b-list-group-item>
+  <b-list-group-item><a href="#">初めまして！自己紹介</a></b-list-group-item>
+  <b-list-group-item><a href="#">こんな風に働きたい</a></b-list-group-item>
+  <b-list-group-item><a href="#">私の強みはこれ！</a></b-list-group-item>
 </b-list-group>
   </b-modal>
   </nav> 
