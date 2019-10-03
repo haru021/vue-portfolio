f596e0294e7ebc31a620e91feba654ad1e88996e
小林　萌 <haru.gt.021@gmail.com>
2019-10-03 14:07:52 +0900

    自己紹介のファイルを変更

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 //ルート用のコンポーネントを読み込む
 import Home from '@/views/Home.vue'
-import Product from '@/views/Product.vue'
+import Profile from '@/views/Profile.vue'
 // import header from '@/views/header.vue'
 //Vuexと同様で最初にプラグインとして登録
 Vue.use(VueRouter)
@@ -13,7 +13,7 @@ const router = new VueRouter({
     //URLのパスと紐づくコンポーネントをマッピング
     routes:[
         {path: '/', component: Home},
-        {path: '/product', component: Product}
+        {path: '/Profile', component: Profile}
     ]
 })
 
