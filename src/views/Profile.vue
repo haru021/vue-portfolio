981d80108b6f209eda05ed3f754827e3a3493d08
小林　萌 <haru.gt.021@gmail.com>
2019-10-30 14:18:46 +0900

    個人情報を伏せるように修正

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,8 +5,7 @@
             <h1 class="text-left">自己紹介</h1>
         <b-card-group deck>
             <b-card header="基本情報" class="text-left">
-        <b-card-text><h2>小林萌</h2>
-        <p>生年月日：1992年9月1日</p>
+        <b-card-text><h2>晴(haru)</h2>
         <p>年齢：27歳</p>
         <p>学歴：九州産業大学国際文化学部日本文化学科2016年卒業</p>
         <p>職歴：携帯販売・Webデザイナー</p>
